6a570ed6893b673dd2e633e05a3740874a6b7381
Chikkorde <120329211+Chikkorde@users.noreply.github.com>
2023-01-23 08:56:03 +0530

    first commit

--- script.js
+++ script.js
@@ -10,14 +10,65 @@ function convertToRoman(num) {
     };
 
   //your code here
+    if (typeof num !== 'number') 
+    return false; 
+    
+    var digits = String(+num).split(""),
+    key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+    "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+    "","I","II","III","IV","V","VI","VII","VIII","IX"],
+    roman_num = "",
+    i = 3;
+    while (i--)
+    roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num;
+    return Array(+digits.join("") + 1).join("M") + roman_num;
+    }
+    
+   
 
-}
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
 
 // do not edit below this line
 module.exports = convertToRoman
+
+
+
+
+/*const intToRoman = (num) => {
+  const map = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let result = '';
+  
+  for (key in map) {  
+    const repeatCounter = Math.floor(num / map[key]);
+    
+    if (repeatCounter !== 0) {
+      result += key.repeat(repeatCounter);
+    }
+    
+    num %= map[key];
+    
+    if (num === 0) return result;
+  }
+  
+  return result;
+};*/
